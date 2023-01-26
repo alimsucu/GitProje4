744e7b5adc404c06e81b4a6b0aacd200eefad448
alimsucu <alimsucu7.5@gmail.com>
2023-01-26 19:29:55 +0300

    second task was done.

--- src/Gun1/school.java
+++ src/Gun1/school.java
@@ -2,6 +2,11 @@ package Gun1;
 
 public class school {
     public static void main(String[] args) {
+
         System.out.println("merhaba 1");
+
+        System.out.println("merhaba hüzün");
+
+        System.out.println("second task was done.");
     }
 }
