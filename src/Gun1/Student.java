7b61638f2f6f9f69784534f8bd28805a98c45032
alimsucu <alimsucu7.5@gmail.com>
2023-01-26 20:09:30 +0300

    fifth task was done.

--- src/Gun1/Student.java
+++ src/Gun1/Student.java
@@ -4,5 +4,7 @@ public class Student {
     public static void main(String[] args) {
 
         System.out.println("innallahe meassabirin");
+
+        System.out.println("vicdan azabı yaşanmıyor");
     }
 }
