df1741712b3e74da7ba683012d8dfd5cf17f2917
alimsucu <alimsucu7.5@gmail.com>
2023-01-26 19:38:50 +0300

    forth task was done.

--- src/Gun1/school.java
+++ src/Gun1/school.java
@@ -3,8 +3,6 @@ package Gun1;
 public class school {
     public static void main(String[] args) {
 
-        System.out.println("merhaba 1");
-
         System.out.println("merhaba hüzün");
         System.out.println("second task was done.");
 
